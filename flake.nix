e103af1bedf82c00a60cc419533771957899a716
Mark Bishop <mbishop@duosecurity.com>
2024-02-02 14:45:14 -0500

    One more fixup for colors

--- flake.nix
+++ flake.nix
@@ -32,200 +32,206 @@
       lib = nixpkgs.lib;
       nvimlib = neovim-flake.lib.nvim;
       configModule = args: {
-        config.vim = {
-          theme.enable = lib.mkForce false;
-          theme.name = "gruvbox";
-          languages.nix.enable = true;
-          filetree.nvimTreeLua.enable = false;
-          tabline.nvimBufferline.enable = true;
-          autopairs.enable = false;
-          languages = {
-            sql.lsp.enable = false;
-          };
-          lsp = {
-            formatOnSave = true;
-          };
-          treesitter.grammars = [
-            pkgs.vimPlugins.nvim-treesitter-parsers.php
-          ];
-          preventJunkFiles = true;
-          startPlugins = with pkgs.vimPlugins; [
-            hop-nvim
-            ranger-vim
-            kommentary
-            telescope-fzf-native-nvim
-            fzf-vim
-            haskell-tools-nvim
-            nvim-dap
-            nvim-surround
-            nvim-ts-context-commentstring
-            symbols-outline-nvim
-            idris-vim
-            # copilot-vim
-            # vim-buftabline
-            #base16-vim
-            nvim-base16
-            fugitive
-            ultisnips
-            vimwiki
-          ];
-          nnoremap = {
-            "-" = ":bp<CR>";
-            "=" = ":bn<CR>";
-            "<C-d>" = ":w<CR>";
-            "F" = ":RangerEdit<CR>";
-            "<leader>k" = "\"zyiw :Rg <C-r>z<CR>";
-            "<C-p>" = "<Esc>:Rg ";
-            "<leader>G" = "<cmd> Telescope git_files<CR>";
-            "<leader>L" = "<cmd> Lines<CR>";
-            "c/" = ":nohlsearch<CR>";
-            "<F4>" = ":SymbolsOutline<CR>";
-            "<leader>ad" = "<cmd>let g:formatsave=v:false<CR>";
-            "<leader>ae" = "<cmd>let g:formatsave=v:true<CR>";
-            "<leader>ww" = "<cmd>VimwikiIndex<CR>";
-          };
-          inoremap = {
-            "<C-k>" = "<Esc>";
-            "<C-d>" = "<Esc>:w<CR>";
-          };
-          luaConfigRC = {
-            myHop = nvimlib.dag.entryAfter ["hop"] ''
-              local hop = require('hop')
-              hop.setup()
-              vim.keymap.set("", '<c-l>', function()
-                hop.hint_words()
-              end, {remap=true})
-            '';
-            bufferline = nvimlib.dag.entryAfter ["nvimBufferline"] ''
-              require('bufferline').setup({
-                options = {
-                  show_close_icon = false,
-                  show_buffer_close_icons = false,
-                  show_buffer_icons = false,
-                  indicator = {
-                    style = 'none'
+        config.vim = lib.mkMerge [
+          {
+            theme.enable = lib.mkForce false;
+            theme.name = "gruvbox";
+            languages.nix.enable = true;
+            filetree.nvimTreeLua.enable = false;
+            tabline.nvimBufferline.enable = true;
+            autopairs.enable = false;
+            languages = {
+              sql.lsp.enable = false;
+            };
+            lsp = {
+              formatOnSave = true;
+            };
+            treesitter.grammars = [
+              pkgs.vimPlugins.nvim-treesitter-parsers.php
+            ];
+            preventJunkFiles = true;
+            startPlugins = with pkgs.vimPlugins; [
+              hop-nvim
+              ranger-vim
+              kommentary
+              telescope-fzf-native-nvim
+              fzf-vim
+              haskell-tools-nvim
+              nvim-dap
+              nvim-surround
+              nvim-ts-context-commentstring
+              symbols-outline-nvim
+              idris-vim
+              # copilot-vim
+              # vim-buftabline
+              #base16-vim
+              nvim-base16
+              fugitive
+              ultisnips
+              vimwiki
+            ];
+            nnoremap = {
+              "-" = ":bp<CR>";
+              "=" = ":bn<CR>";
+              "<C-d>" = ":w<CR>";
+              "F" = ":RangerEdit<CR>";
+              "<leader>k" = "\"zyiw :Rg <C-r>z<CR>";
+              "<C-p>" = "<Esc>:Rg ";
+              "<leader>G" = "<cmd> Telescope git_files<CR>";
+              "<leader>L" = "<cmd> Lines<CR>";
+              "c/" = ":nohlsearch<CR>";
+              "<F4>" = ":SymbolsOutline<CR>";
+              "<leader>ad" = "<cmd>let g:formatsave=v:false<CR>";
+              "<leader>ae" = "<cmd>let g:formatsave=v:true<CR>";
+              "<leader>ww" = "<cmd>VimwikiIndex<CR>";
+            };
+            inoremap = {
+              "<C-k>" = "<Esc>";
+              "<C-d>" = "<Esc>:w<CR>";
+            };
+            luaConfigRC = {
+              myHop = nvimlib.dag.entryAfter ["hop"] ''
+                local hop = require('hop')
+                hop.setup()
+                vim.keymap.set("", '<c-l>', function()
+                  hop.hint_words()
+                end, {remap=true})
+              '';
+              bufferline = nvimlib.dag.entryAfter ["nvimBufferline"] ''
+                require('bufferline').setup({
+                  options = {
+                    show_close_icon = false,
+                    show_buffer_close_icons = false,
+                    show_buffer_icons = false,
+                    indicator = {
+                      style = 'none'
+                    },
+                    offsets = {
+                      separator = false
+                    },
+                    seperator_style = 'thin'
+                  }
+                })
+              '';
+              hoverForDiagnostics = nvimlib.dag.entryAnywhere ''
+                vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})]]
+              '';
+              surround = nvimlib.dag.entryAnywhere ''
+                require("nvim-surround").setup({})
+              '';
+              treesitterCommentContext = nvimlib.dag.entryAnywhere ''
+                require('nvim-treesitter.configs').setup {
+                  ensure_installed = {},
+                  auto_install = false,
+                  context_commentstring = {
+                    enable = true,
                   },
-                  offsets = {
-                    separator = false
-                  },
-                  seperator_style = 'thin'
                 }
-              })
-            '';
-            hoverForDiagnostics = nvimlib.dag.entryAnywhere ''
-              vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})]]
-            '';
-            surround = nvimlib.dag.entryAnywhere ''
-              require("nvim-surround").setup({})
-            '';
-            treesitterCommentContext = nvimlib.dag.entryAnywhere ''
-              require('nvim-treesitter.configs').setup {
-                ensure_installed = {},
-                auto_install = false,
-                context_commentstring = {
-                  enable = true,
-                },
-              }
-            '';
-            symbolsOutline = nvimlib.dag.entryAnywhere ''
-              require("symbols-outline").setup()
-            '';
-            telescopeFzf = nvimlib.dag.entryAfter ["telescope"] ''
-              require('telescope').setup {
-                extensions = {
-                  fzf = {
-                    fuzzy = true,                    -- false will only do exact matching
-                    override_generic_sorter = true,  -- override the generic sorter
-                    override_file_sorter = true,     -- override the file sorter
-                    case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                                     -- the default case_mode is "smart_case"
+              '';
+              symbolsOutline = nvimlib.dag.entryAnywhere ''
+                require("symbols-outline").setup()
+              '';
+              telescopeFzf = nvimlib.dag.entryAfter ["telescope"] ''
+                require('telescope').setup {
+                  extensions = {
+                    fzf = {
+                      fuzzy = true,                    -- false will only do exact matching
+                      override_generic_sorter = true,  -- override the generic sorter
+                      override_file_sorter = true,     -- override the file sorter
+                      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                                       -- the default case_mode is "smart_case"
+                    }
                   }
                 }
-              }
-              require('telescope').load_extension('fzf')
-            '';
-            haskellToolsConfiguration = ''
-              -- Haskell config
-              lspconfig.hls.setup {
-                capabilities = capabilities;
-                on_attach = default_on_attach;
-                cmd = { "haskell-language-server-wrapper", "--lsp", "-j", "2" };
-                root_dir = lspconfig.util.root_pattern("hie.yaml", "stack.yaml", ".cabal", "cabal.project", "project.yaml");
-              }
-            '';
-            treesitterWarningFix = nvimlib.dag.entryAnywhere ''
-              require('ts_context_commentstring').setup {}
-              vim.g.skip_ts_context_commentstring_module = true
-            '';
-            base16-colors = nvimlib.dag.entryAnywhere ''
-              vim.cmd('colorscheme ${args.colorscheme}')
-            '';
-            base16-comment-override =
-              nvimlib.dag.entryAfter ["base16-colors"]
-              ''
-                local color = require('base16-colorscheme').colors.base04
-                vim.cmd('highlight Comment guifg=' .. color)
-                vim.cmd('highlight TSComment guifg=' .. color)
+                require('telescope').load_extension('fzf')
               '';
-          };
-          configRC = {
-            lineNumbers = nvimlib.dag.entryAnywhere ''
-              set norelativenumber
-            '';
-            /*
-               noctuTheme = nvimlib.dag.entryAfter ["lineNumbers"] (builtins.readFile "${noctu}/colors/noctu.vim");
-            force16Clors = nvimlib.dag.entryAfter ["noctuTheme"] ''
-              set notermguicolors t_Co=16
-            '';
-            */
-            buftabLineHighlighting = nvimlib.dag.entryAnywhere ''
-              hi clear BufTabLineCurrent
-              hi clear BufTabLineActive
-              hi clear ColorColumn
-              hi link BufTabLineCurrent GruvboxYellowSign
-              hi BufTabLineHidden guibg=#282828 ctermfg=214 ctermbg=237 guifg=#a89984
-              hi BufTabLineActive guibg=#3c3836 ctermfg=214 ctermbg=237 guifg=#fabd2f
-              hi BufTabLineCurrent guibg=#504945 ctermfg=214 ctermbg=237 guifg=#fabd2f gui=bold cterm=bold
-              hi BufTabLineFill guibg=#282828 ctermfg=214 ctermbg=237 guifg=#a89984
-              hi link BufTabLineActive GruvboxYellow
+              haskellToolsConfiguration = ''
+                -- Haskell config
+                lspconfig.hls.setup {
+                  capabilities = capabilities;
+                  on_attach = default_on_attach;
+                  cmd = { "haskell-language-server-wrapper", "--lsp", "-j", "2" };
+                  root_dir = lspconfig.util.root_pattern("hie.yaml", "stack.yaml", ".cabal", "cabal.project", "project.yaml");
+                }
+              '';
+              treesitterWarningFix = nvimlib.dag.entryAnywhere ''
+                require('ts_context_commentstring').setup {}
+                vim.g.skip_ts_context_commentstring_module = true
+              '';
+            };
+            configRC = {
+              lineNumbers = nvimlib.dag.entryAnywhere ''
+                set norelativenumber
+              '';
+              /*
+                 noctuTheme = nvimlib.dag.entryAfter ["lineNumbers"] (builtins.readFile "${noctu}/colors/noctu.vim");
+              force16Clors = nvimlib.dag.entryAfter ["noctuTheme"] ''
+                set notermguicolors t_Co=16
+              '';
+              */
+              buftabLineHighlighting = nvimlib.dag.entryAnywhere ''
+                hi clear BufTabLineCurrent
+                hi clear BufTabLineActive
+                hi clear ColorColumn
+                hi link BufTabLineCurrent GruvboxYellowSign
+                hi BufTabLineHidden guibg=#282828 ctermfg=214 ctermbg=237 guifg=#a89984
+                hi BufTabLineActive guibg=#3c3836 ctermfg=214 ctermbg=237 guifg=#fabd2f
+                hi BufTabLineCurrent guibg=#504945 ctermfg=214 ctermbg=237 guifg=#fabd2f gui=bold cterm=bold
+                hi BufTabLineFill guibg=#282828 ctermfg=214 ctermbg=237 guifg=#a89984
+                hi link BufTabLineActive GruvboxYellow
 
-              hi clear BufferLineBufferSelected
-              hi clear BufferLineBufferVisible
-              hi BufferLineBufferSelected guibg=#504945 ctermfg=214 ctermbg=237 guifg=#fabd2f gui=bold cterm=bold
-              hi BufferLineBufferVisible guibg=#3c3836 ctermfg=214 ctermbg=237 guifg=#fabd2f
-            '';
-            ai = nvimlib.dag.entryAnywhere ''
-              let g:copilot_no_tab_map = v:true
-              imap <silent><script><expr> <C-J> copilot#Accept("\<CR>")
-              let g:copilot_no_tab_map = v:true
-            '';
-            direnv = nvimlib.dag.entryAfter ["luaScript"] ''
-              if filereadable($DIRENV_EXTRA_VIMRC)
-                source $DIRENV_EXTRA_VIMRC
-              endif
-            '';
-            whitespace = nvimlib.dag.entryAnywhere ''
-              function <SID>StripTrailingWhitespace()
-                  " Preparation: save last search, and cursor position.
-                  let l:_s=@/
-                  let l:l = line('.')
-                  let l:c = col('.')
-                  " Do the business:
-                  %s/\s\+$//e
-                  " Clean up: restore previous search history, and cursor position
-                  let @/=l:_s
-                  call cursor(l:l, l:c)
-              endfunction
-              nmap <silent> <leader>t :call <SID>StripTrailingWhitespace()<CR>
-            '';
-            ultisnips = nvimlib.dag.entryAnywhere ''
-              let g:UltiSnipsExpandTrigger='<c-a>'
-              let g:UltiSnipsJumpForwardTrigger='<c-l>'
-              let g:UltiSnipsJumpBackwardTrigger='<c-h>'
-              let g:UltiSnipsSnippetDirectories = ["UltiSnips", "${./snippets}"]
-            '';
-          };
-        };
+                hi clear BufferLineBufferSelected
+                hi clear BufferLineBufferVisible
+                hi BufferLineBufferSelected guibg=#504945 ctermfg=214 ctermbg=237 guifg=#fabd2f gui=bold cterm=bold
+                hi BufferLineBufferVisible guibg=#3c3836 ctermfg=214 ctermbg=237 guifg=#fabd2f
+              '';
+              ai = nvimlib.dag.entryAnywhere ''
+                let g:copilot_no_tab_map = v:true
+                imap <silent><script><expr> <C-J> copilot#Accept("\<CR>")
+                let g:copilot_no_tab_map = v:true
+              '';
+              direnv = nvimlib.dag.entryAfter ["luaScript"] ''
+                if filereadable($DIRENV_EXTRA_VIMRC)
+                  source $DIRENV_EXTRA_VIMRC
+                endif
+              '';
+              whitespace = nvimlib.dag.entryAnywhere ''
+                function <SID>StripTrailingWhitespace()
+                    " Preparation: save last search, and cursor position.
+                    let l:_s=@/
+                    let l:l = line('.')
+                    let l:c = col('.')
+                    " Do the business:
+                    %s/\s\+$//e
+                    " Clean up: restore previous search history, and cursor position
+                    let @/=l:_s
+                    call cursor(l:l, l:c)
+                endfunction
+                nmap <silent> <leader>t :call <SID>StripTrailingWhitespace()<CR>
+              '';
+              ultisnips = nvimlib.dag.entryAnywhere ''
+                let g:UltiSnipsExpandTrigger='<c-a>'
+                let g:UltiSnipsJumpForwardTrigger='<c-l>'
+                let g:UltiSnipsJumpBackwardTrigger='<c-h>'
+                let g:UltiSnipsSnippetDirectories = ["UltiSnips", "${./snippets}"]
+              '';
+            };
+          }
+          (lib.mkIf (builtins.hasAttr "colorscheme" args) {
+            luaConfigRC = {
+              base16-colors = nvimlib.dag.entryAnywhere ''
+                vim.cmd('colorscheme ${args.colorscheme}')
+              '';
+              base16-comment-override =
+                nvimlib.dag.entryAfter ["base16-colors"]
+                ''
+                  local color = require('base16-colorscheme').colors.base04
+                  vim.cmd('highlight Comment guifg=' .. color)
+                  vim.cmd('highlight TSComment guifg=' .. color)
+                '';
+            };
+          })
+        ];
       };
 
       baseNeovim = neovim-flake.packages.${system}.maximal;
@@ -235,12 +241,13 @@
           modules = [(configModule args)];
         };
     in {
+      ### TODO export a function that builds an nvim package based on config arguments
       packages = rec {
         neovim = neovimExtended {
           colorscheme = "base16-gruvbox-material-dark-hard";
         };
         default = neovim;
-        other = neovim;
+        colorless = neovimExtended {};
       };
     });
 }
