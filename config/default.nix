96134dd1a0888dc89dee2e2eff7051e75576948d
Mark Bishop <mbishop@duosecurity.com>
2025-03-25 17:39:13 -0400

    Start rewrite

---
+++ config/default.nix
@@ -0,0 +1,14 @@
+{pkgs}: let
+  configDir = pkgs.stdenv.mkDerivation {
+    name = "nvim-lua-lazy-configs";
+    src = ./lua; # NOTE: the installPhase commands relative to this dir
+    installPhase = ''
+      mkdir -p $out/lua
+      cp ./init.lua $out/
+      cp -r ./scripts $out/lua/
+    '';
+  };
+in ''
+  set rtp+=${configDir}
+  source ${configDir}/init.lua
+''
---
+++ config/lua/scripts/settings.lua
@@ -0,0 +1,32 @@
+local o = vim.opt
+
+o.compatible = false
+vim.cmd("autocmd!")
+o.autoindent = true
+o.autoread = true
+o.autowrite = true
+o.backspace="indent,eol,start"
+o.clipboard="unnamedplus"
+o.completeopt="menuone"
+o.conceallevel=2
+o.directory=/tmp
+o.expandtab = true
+o.fillchars += "vert:│"
+o.hidden = true
+o.history= 50
+o.hlsearch = true
+o.incsearch = true
+o.laststatus= 2
+o.mouse -= a
+o.showmode = true
+o.swapfile = false
+o.number = true
+o.ruler = true
+o.shiftwidth = 4
+o.softtabstop = 4
+o.tabstop = 4
+o.termguicolors = true
+o.undodir = ~/.vim/undodir
+o.undofile = true
+o.updatetime = 500
+o.wildmode = "list:longest,full"
