eed5bca10c1ebd4e1268239f0264f9f235a974e4
Mark Bishop <mbishop@duosecurity.com>
2025-03-27 11:59:14 -0400

    Move LSP configuration into its own file

---
+++ config/lua/scripts/lsp.lua
@@ -0,0 +1,72 @@
+return {
+    {
+        "scripts.lsp.pyright",
+        load = function()
+            require("scripts.lsp.pyright")
+        end,
+        ft = "python",
+    },
+    {
+        "scripts.lsp.nixd",
+        load = function()
+            require("scripts.lsp.nixd")
+        end,
+        ft = "nix",
+    },
+    {
+        "scripts.lsp.gopls",
+        load = function()
+            require("scripts.lsp.gopls")
+        end,
+        ft = { "go", "gomod" },
+    },
+    {
+        "scripts.lsp.ccls",
+        load = function()
+            require("scripts.lsp.ccls")
+        end,
+        ft = { "c", "cpp", "objc", "objcpp", "cuda" },
+    },
+    {
+        "scripts.lsp.ts_ls",
+        load = function()
+            require("scripts.lsp.ts_ls")
+        end,
+        ft = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+        },
+    },
+    {
+        "scripts.lsp.lua-ls",
+        load = function()
+            require("scripts.lsp.lua-ls")
+        end,
+        ft = "lua",
+    },
+    {
+        "scripts.lsp.rust_analyzer",
+        load = function()
+            require("scripts.lsp.rust_analyzer")
+        end,
+        ft = "rust",
+    },
+    {
+        "scripts.lsp.hls",
+        load = function()
+            require("scripts.lsp.hls")
+        end,
+        ft = { "haskell", "cabal", "stack" },
+    },
+    {
+        "scripts.lsp.gdscript",
+        load = function()
+            require("scripts.lsp.gdscript")
+        end,
+        ft = { "gd", "gdscript" },
+    },
+}
